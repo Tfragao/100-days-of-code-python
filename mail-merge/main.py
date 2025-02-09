bc16dbd071dcb67ad77d774d5b66774fa8d8032c
taison <fragaot@gmail.com>
2025-02-09 18:53:46 +0100

    output folder updated

--- mail-merge/main.py
+++ mail-merge/main.py
@@ -11,5 +11,5 @@ for name in names:
     cleaned_name = name.strip() # Remove whitespace/newline characters
     personalized_letter = "".join([line.replace("[name]", cleaned_name) for line in template])
     #save the letter with a proper filename
-    with open(f"./output/readyTosend/letter_for_{cleaned_name}.txt", mode="w") as output_file:
+    with open(f"./output/ready_to_send/letter_for_{cleaned_name}.txt", mode="w") as output_file:
         output_file.write(personalized_letter)
