6d54c14dc806ac62095d239860e77a95d445b407
taison <fragaot@gmail.com>
2025-02-26 21:53:18 +0100

    [password-manager]:label, entries and buttons added.

--- password-manager/main.py
+++ password-manager/main.py
@@ -6,13 +6,33 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.minsize(width=800, height=400)
-window.config(pady=20, padx=20)
+window.config(pady=50, padx=50)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack() # pack for now, when adding more elements to the window we can use grid() instead
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+website_entry = Entry(width=33)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_username_label = Label(text="Email/Username:")
+email_username_label.grid(column=0, row=2)
+email_username_entry = Entry(width=33)
+email_username_entry.grid(column=1, row=2, columnspan=2)
+
+pass_label = Label(text="Password:")
+pass_label.grid(column=0, row=3)
+password_entry = Entry(width=15)
+password_entry.grid(column=1, row=3)
+
+generate_pass_btn = Button(text="Generate Password")
+generate_pass_btn.grid(column=2, row=3)
+
+add_btn = Button(text="Add", width=36)
+add_btn.grid(column=1, row=4, columnspan=2)
 
 
 window.mainloop()
