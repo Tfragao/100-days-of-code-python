23d6054fcb3e42e37d62b82a068c915c864ca700
taison <fragaot@gmail.com>
2025-03-07 07:47:56 +0100

    [quizzler-app]:ui set up[done] Getting question in the canvas [done].

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -1,2 +1,42 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=400, height=300, bg="white")
+        self.question_text = self.canvas.create_text(
+            200,
+            150,
+            width=280,
+            text="some silly test",
+            font=("Arial", 20, "italic"),
+            fill=THEME_COLOR
+        )
+        self.canvas.grid(column=0, row=1, columnspan=2,pady=20)
+
+        self.false_img = PhotoImage(file="images/false.png")
+        self.false_btn = Button(image=self.false_img, highlightthickness=0)
+        self.false_btn.config(padx=50, pady=50)
+        self.false_btn.grid(column=0, row=2)
+
+        self.true_img = PhotoImage(file="images/true.png")
+        self.true_btn = Button(image=self.true_img, highlightthickness=0)
+        self.true_btn.grid(column=1, row=2)
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
